5c1672d803f8322e69a635ebc4438d125b599d70
William Kann <william.l.kann@gmail.com>
2017-11-20 16:50:43 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,15 @@
 
 def display_board
   ttt_line1 = ["   ", "|", "    ", "|", "    "]
+  ttt_line2 = "-----------"
+  ttt_line3 = ["   ", "|", "    ", "|", "    "]
+  ttt_line4 = "-----------"
+  ttt_line5 = ["   ", "|", "    ", "|", "    "]
   puts ttt_line1.join
+  puts ttt_line2
+  puts ttt_line3.join
+  puts ttt_line4
+  puts ttt_line5.join
 end
 
 puts display_board
